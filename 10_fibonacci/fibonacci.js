62738dfb33eeab6a7f4827db31de9d99199d4882
Reach <brianelliott2015@gmail.com>
2023-06-01 18:21:05 -0400

    complete excercize 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (typeof x === "string")
+        x=parseInt(x);
+    if(x <= 0)
+        return "OOPS"
+    if(x===1 || x===2)
+        return 1
+    let a = 1;
+    let b = 1;
+    let sum;
+    for(let i = 2; i < x; i++){
+        sum = a+b;
+        a = b;
+        b = sum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
