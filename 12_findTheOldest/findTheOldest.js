5258a2735a6828086b4606f21af19a7c20fdf6e1
Reach <brianelliott2015@gmail.com>
2023-06-01 19:42:53 -0400

    Complete excercize 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    sortedPeople = people.sort((a,b) => {
+        if (!('yearOfDeath' in a)){
+            a.yearOfDeath = new Date().getFullYear();
+        }
+        if (!('yearOfDeath' in b)){
+            b.yearOfDeath = new Date().getFullYear();
+        }
+        aAge = a.yearOfDeath - a.yearOfBirth;
+        bAge = b.yearOfDeath - b.yearOfBirth;
+        return aAge > bAge ? -1 : 1;
+    });
+    return sortedPeople[0];
 };
 
 // Do not edit below this line
