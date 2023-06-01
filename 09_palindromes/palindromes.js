1a0750f070b67932825c8dab85c0cab276b4e315
Reach <brianelliott2015@gmail.com>
2023-06-01 16:57:12 -0400

    Complete excercize 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
+const palindromes = function (rawString) {
+    var punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ ';
+    
+    //remove punctuation
+    var rawLetters = rawString.split("");
+    var cleanLetters = rawLetters.filter((letter) => 
+                        punctuation.indexOf(letter) === -1);
 
+    var cleanString = cleanLetters.join("");
+
+    //remove capital letters
+    cleanString=cleanString.toLowerCase();
+    cleanLetters=cleanString.split("")
+
+    len = cleanLetters.length;
+    //get rid of center letter if there is one
+    if(len%2){
+        cleanLetters.splice(Math.floor(len/2.0),1)
+        len--;
+    }
+
+    //iterate over string looking for mismatches
+    for(let i = 0; i < len/2; i++) {
+        if(cleanLetters[i] !== cleanLetters[len-i-1])
+            return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
