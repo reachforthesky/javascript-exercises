2942f4acd3493a1290ef7437d89d9e756ccbbd39
Reach <brianelliott2015@gmail.com>
2023-06-01 16:38:26 -0400

    Complete excersize 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((a,b) => a+b,0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a,b) => a*b,1);
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+  acc = x
+  for(let i = 1; i < y; i++){
+    acc=acc*x
+  }
+  return acc;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if(x===1 || x===0)
+    return 1
+  if(x===2)
+    return 2
+	acc = 2;
+  iter = 3;
+  while(iter <= x){
+    acc=acc*iter;
+    iter++;
+  }
+  return acc;
 };
 
 // Do not edit below this line
